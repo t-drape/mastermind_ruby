fd1facd355741098a2178cb75b3599a22a6ed815
t-drape <thebatreview1@gmail.com>
2024-08-05 23:42:26 -0700

    Check of boxes for step 1

--- main.rb
+++ main.rb
@@ -6,27 +6,27 @@
 # “Build the game assuming the computer randomly selects the secret colors, and the human player must guess them.
 #  Remember that you need to give the proper feedback 	on  how good the guess was each turn!”
 
-#     - [ ] Create PlayGame function/class
+#     - [x] Create PlayGame function/class
 #         - [x] Set ACCEPTABLE_COLORS to six colors, (blue, red, orange, yellow, purple, green)
 #         - [x] Let computer generate random four color code, (blue, red, orange, yellow, purple, green)
-#         - [ ] Define PlayGame to PlayRound 12 times
-#             - [ ] If winner, exit loop, print congratulatory message
-#             - [ ] Else, continue until 12, then print loser message
-#         - [ ] PlayRound
-#             - [ ] Set black_dot_variable to 0
-#             - [ ] Set white_dot_variable to 0
-#             - [ ] Get User Guess
-#             - [ ] Compare User Guess to random color code
-#             - [ ] If equal, declare winner
-#             - [ ] Else,
-#                 - [ ] If any color from guess in color code,
-#                     - [ ] For each color in user guess,
-#                         - [ ] If guess is correct color and position,
-#                             - [ ] Increase black_dot by one
-#                         - [ ] Else if guess is correct color but wrong position,
-#                             - [ ] Increase white_dot by one
-#                         - [ ] Else do nothing
-#                 - [ ] Else, continue loop
+#         - [x] Define PlayGame to PlayRound 12 times
+#             - [x] If winner, exit loop, print congratulatory message
+#             - [x] Else, continue until 12, then print loser message
+#         - [x] PlayRound
+#             - [x] Set black_dot_variable to 0
+#             - [x] Set white_dot_variable to 0
+#             - [x] Get User Guess
+#             - [x] Compare User Guess to random color code
+#             - [x] If equal, declare winner
+#             - [x] Else,
+#                 - [x] If any color from guess in color code,
+#                     - [x] For each color in user guess,
+#                         - [x] If guess is correct color and position,
+#                             - [x] Increase black_dot by one
+#                         - [x] Else if guess is correct color but wrong position,
+#                             - [x] Increase white_dot by one
+#                         - [x] Else do nothing
+#                 - [x] Else, continue loop
 # Step 2:
 # “Now refactor your code to allow the human player to choose whether they want to be the creator of the secret code
 # or the guesser.”
